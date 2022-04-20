a3f75ca2370ccedfafc154e2ff93d19e037f164c
108Engineering <104067346+108Engineering@users.noreply.github.com>
2022-04-20 17:08:45 +0700

    Track Way

--- main.ts
+++ main.ts
@@ -7,17 +7,35 @@ function doFW () {
     Find_Line.Center,
     1,
     30,
-    100,
+    60,
     0.015,
     0.01
     )
     PTKidsBITRobot.ForwardTIME(
-    200,
-    20,
+    250,
+    30,
     50,
     0.015,
     0.01
     )
+}
+function doTurnRight () {
+    PTKidsBITRobot.TurnLINE(
+    Turn_Line.Right,
+    40,
+    Turn_Sensor.Center,
+    200
+    )
+    basic.pause(1000)
+}
+function doTurnLeft () {
+    PTKidsBITRobot.TurnLINE(
+    Turn_Line.Left,
+    40,
+    Turn_Sensor.Center,
+    200
+    )
+    basic.pause(1000)
 }
 function doUnGrip () {
     music.playTone(330, music.beat(BeatFraction.Whole))
@@ -46,16 +64,29 @@ function doGRIP () {
 input.onButtonPressed(Button.B, function () {
     music.playTone(262, music.beat(BeatFraction.Whole))
     basic.pause(1000)
+    doUP()
+    doUnGrip()
+    music.playTone(622, music.beat(BeatFraction.Whole))
     doFW()
+    doFW()
+    doTurnLeft()
     doFW()
     doUDW()
     doGRIP()
     doUP()
     doFW()
+    doTurnRight()
+    doFW()
+    doTurnRight()
+    doFW()
+    doFW()
+    doTurnRight()
+    doFW()
     doFW()
     doUDW()
     doUnGrip()
     doUP()
+    music.playMelody("C D E F G A B C5 ", 300)
 })
 function doUDW () {
     PTKidsBITRobot.servoWrite(Servo_Write.P8, 92)
